547bac5b4e93435ddb9a9467e096f383d9c23e81
oldchevy <nick.olszowy@gmail.com>
2016-08-01 21:11:26 -0700

    Add textures, refine motions, and line dance

--- src/behaviorMethods.js
+++ src/behaviorMethods.js
@@ -9,10 +9,10 @@
 
     for (var i = 0, l = elements.length; i < l; i++) {
       if (i % 2 !== 0) {
-        elements[i].setPosition(height - 100, forwards);
+        elements[i].setPosition(height - 300, forwards);
         forwards += Math.min($('body').width() / elements.length, 100);
       } else {
-        elements[i].setPosition(height - 100, backwards);
+        elements[i].setPosition(height - 300, backwards);
         backwards -= Math.min($('body').width() / elements.length, 100);
       }
     }
