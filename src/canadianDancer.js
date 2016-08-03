d80024568daf5e76e8d208e0341ed4a37ae50398
oldchevy <nick.olszowy@gmail.com>
2016-08-02 19:48:03 -0700

    Add fart effects

--- src/canadianDancer.js
+++ src/canadianDancer.js
@@ -3,7 +3,7 @@ var CanadianDancer = function(top, left, timeBetweenSteps) {
   this.$node.addClass('canadian');
   this.id = Math.random().toString();
   this.$node.attr('id', this.id);
-  this.$node.append('<div class ="upper-half"><div class="head"><div class="head-top"><div class="hair"></div><div class="hair-right"></div><div class="eye-left"></div><div class="eye-right"></div>   </div>   <div class="head-bottom"></div>  </div>  <div class="torso"></div> <div class="arm left-arm"></div><div class="arm right-arm"></div></div><div class="lower-half"><div class="leg left-leg"></div><div class="leg right-leg"></div></div>'); 
+  this.$node.append('<div class ="upper-half"><div class="head"><div class="head-top"><div class="hair"></div><div class="hair-right"></div><div class="eye-left"></div><div class="eye-right"></div>   </div>   <div class="head-bottom"></div>  </div>  <div class="torso"></div> <div class="arm left-arm"></div><div class="arm right-arm"></div></div><div class="lower-half"><div class="fart"></div><div class="leg left-leg"></div><div class="leg right-leg"></div></div>'); 
   this.mouseListener();
 };
 CanadianDancer.prototype = Object.create(Dancer.prototype);
